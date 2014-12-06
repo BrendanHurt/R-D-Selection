0b1fa9ab616e60f367c2a32f6adc4c17103b378d
BrendanHurt <brendan.Frodado@gmail.com>
2014-12-05 16:54:36 -0800

    Unit Behavior
    Getting started on creating a behavior tree for the units

---
+++ WaifuFrenzy/Assets/WaifuFrenzy/SelectionRD/Scripts/Units/UnitBehavior.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnitBehavior : MonoBehaviour
+{
+	List<Behavior*> Children;
+
+	void Update ()
+	{
+		
+	}
+}
